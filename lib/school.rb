6969a8f5470561929896139a21c8c1809780e445
Chandler Hulstrom <chulstro11@gmail.com>
2020-05-06 13:48:27 -0600

    Made iteration 2 operate

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,13 @@
+
+
 class School
+  @time = {9 => "9:00", 10 => "10:00", 11 => "11:00",
+        12 => "12:00",
+        13 => "13:00",
+        14 => "14:00",
+        15 => "15:00",
+        16 => "16:00"}
+
   attr_reader :hours_in_school_day,
               :start_time,
               :student_names
@@ -8,4 +17,15 @@ class School
     @start_time = start_time
     @student_names = []
   end
+
+  def add_student_name (name)
+    @student_names << name
+  end
+
+  def end_time
+    time = @start_time.to_i + @hours_in_school_day
+    time.to_s.concat(":00")
+  end
+
+
 end
